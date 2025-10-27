ee55ba00ad633c21696107ccbb1f0b98b108d89e
Yousef <yza@andrew.cmu.edu>
2025-10-27 12:31:02 +0300

    b

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -242,5 +242,27 @@ export default function QueryProcessor(query: string): string {
     return anagrams.join(', ');
   }
 
+  // Handle Scrabble score queries
+  const scrabbleMatch = query.match(/what is the scrabble score of (\w+)/i);
+  if (scrabbleMatch) {
+    const word = scrabbleMatch[1].toLowerCase();
+    const scores: { [key: string]: number } = {
+      'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
+      'd': 2, 'g': 2,
+      'b': 3, 'c': 3, 'm': 3, 'p': 3,
+      'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+      'k': 5,
+      'j': 8, 'x': 8,
+      'q': 10, 'z': 10
+    };
+
+    let total = 0;
+    for (const letter of word) {
+      total += scores[letter] || 0;
+    }
+
+    return total.toString();
+  }
+
   return "";
 }
